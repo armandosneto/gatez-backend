643cbc16fc42c512147ddef949127a8ca93429b6
Armando Soares e Silva Neto <armandosneto@outlook.com>
2022-05-31 01:41:00 -0300

    configuring development environment

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express, { Router, Request, Response } from "express";
+
+const app = express();
+
+const route = Router();
+
+app.use(express.json());
+
+route.get("/", (req: Request, res: Response) => {
+  res.json({ message: "hello world with Typescript" });
+});
+
+app.use(route);
+
+app.listen(3333, () => console.log("Server running on port 3333"));
