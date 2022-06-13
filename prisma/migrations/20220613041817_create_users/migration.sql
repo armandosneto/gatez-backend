e62521c3ac27c131ae9b99a6d8c89d2bbf75014b
Armando Soares e Silva Neto <armandosneto@outlook.com>
2022-06-13 02:19:15 -0300

    changing typeORM for Prisma

---
+++ prisma/migrations/20220613041817_create_users/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
